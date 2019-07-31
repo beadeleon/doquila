522a481dd73ed91927860c19dc53385bc46eb26d
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-07-31 09:46:15 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Bea De Leon Shirt </title>
+</head>
+<body>
+<center><h1> Bea De Leon Shirt</h1>
+<img src="https://cf.shopee.ph/file/546976c214e7ba79db3dca09ee10a789" width="300" height="400" align='left'>
+<h2> Price </h2>
+<h3>?:350</h3>
+<h4> Shipping fee : Pre-order (ships in 10 days)  </h4>
+<h5> Variation :{ White-Xsmall } { White-Small }  { White-Meduim } { White-Large } { Blue-Xsmall } { Blue-Small } { Blue-Meduim } { Blue- Large }</h5>
+<h6> Quantity - 1 + 4998 piece available </h6>
+<h7> Product Description: Bea De Leon the beast in court shirt! Grab yours now! Check out our account for more details   
+#One Big Fight Bea De Leon</h7>
+<p>Beast in court</p>
+<article>
+ <p1> she is a girl when in court she act swagg</p1>
+ </article>
+</body>
+</html>
